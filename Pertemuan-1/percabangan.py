fd35b84bcb7fe1842b0a955c59633fe0a7ceb9c8
unknown <Ariakristallnacht>
2022-11-30 18:09:21 +0700

    ddp praktikum pertemuan 1

---
+++ Pertemuan-1/percabangan.py
@@ -0,0 +1,9 @@
+num = eval(input("masukan angka: "))
+if num > 0:
+    print("bilangan positif")
+elif num < 0:
+    print("bilangan negatif")
+else:
+    print("bilangan nol")
+
+
---
+++ Pertemuan-1/variable.py
@@ -0,0 +1,15 @@
+#Tipe data di pyton:
+# 1. String
+# 2. Boolean
+# 3. Float
+# 4. Integer
+# 5. Hexadecimal
+# 6. DLL
+
+nama = "aria" # string
+umur = 19 # integer
+isStudent  = True # boolean
+ip = 3.50 # float
+
+Umur20thnMendatang = umur + 20 
+print("Nama saya", nama, '. Umur saya 20 tahun mendatang adalah', Umur20thnMendatang)
