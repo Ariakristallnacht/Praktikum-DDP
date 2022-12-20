17be05d65d76b652837735f531d46fd70632b692
unknown <Ariakristallnacht>
2022-12-20 15:53:33 +0700

    praktikum DDP pertemuan 2

---
+++ bilangan.py
@@ -0,0 +1,12 @@
+for i in range(100, 1, -2):
+    print(i, end=", ")
+
+
+#int adalah initeger yang berfungsi untuk meengubah data string menjadi integer(angka)
+#input merupakan fungsi untuk menginput data
+#for merupakan salah satu perintah perulangan
+#i merupakan variabel yang di ambil dari range
+#range merupakan fungsi yang menghasilkan rentan nilai tertentu
+#end berfungsi agar tidak membuat baris baru ketika print()
+#print() berfungsi untuk mencetak hasil
+#nilai 100, 1, dan -2 menyatakan nilai dari 100 sampai 1 lalu dikurang -2
---
+++ persegi.py
@@ -0,0 +1,15 @@
+inp = int(input("Masukkan sebuah bilangan bulat: "))
+
+for i in range(inp):
+    for z in range(inp):
+        print("#", end=" ")
+    print()
+
+#inp merupakan variabel
+#int adalah initeger yang berfungsi untuk meengubah data string menjadi integer(angka)
+#input merupakan fungsi untuk menginput data
+#for merupakan salah satu perintah perulangan
+#i dan z merupakan variabel yang di ambil dari range
+#range merupakan fungsi yang menghasilkan rentan nilai tertentu
+#end berfungsi agar tidak membuat baris baru ketika print()
+#print() berfungsi untuk mencetak hasil
